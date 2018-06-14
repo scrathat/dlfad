eea0a5c4760650a6b00fdb1d6daadb38d3a2e127
Sebastian Kapust <sebastian@kapust.org>
2018-06-14 22:23:56 +0200

    Parallelize augmentation and encoding during lmdb write

--- exercise.2/exercise.2.create.lmdb.py
+++ exercise.2/exercise.2.create.lmdb.py
@@ -1,33 +1,35 @@
+import argparse
 import csv
 import os
 import pickle
 import random
+from functools import partial
+from multiprocessing import Pool, cpu_count
 
-import argparse
 import cv2
 import lmdb
 import numpy as np
 from tqdm import tqdm
 
 
-def augment_brightness(image):
+def augment_brightness(image, angle):
         image1 = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
         random_bright = .25 + np.random.uniform()
         # print(random_bright)
         image1[:, :, 2] = image1[:, :, 2] * random_bright
         image1 = cv2.cvtColor(image1, cv2.COLOR_HSV2RGB)
-        return image1
+        return image1, angle
 
 
-def image_blur(img):
+def image_blur(img, angle):
         # Blur image with random kernel
         kernel_size = random.randint(1, 5)
         if kernel_size % 2 != 1:
                 kernel_size += 1
-        return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+        return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0), angle
 
 
-def add_random_shadow(image):
+def add_random_shadow(image, angle):
         top_y = 320 * np.random.uniform()
         top_x = 0
         bot_x = 160
@@ -52,17 +54,17 @@ def add_random_shadow(image):
                                                             cond0] * random_bright
         image = cv2.cvtColor(image_hls, cv2.COLOR_HLS2RGB)
 
-        return image
+        return image, angle
 
 
 # randomly rotate to simulate camera jitter
-def image_rotate(img):
+def image_rotate(img, angle):
         # Rotate image randomly to simulate camera jitter
         rotate = random.uniform(-1, 1)
         M = cv2.getRotationMatrix2D((img.shape[1] / 2, img.shape[0] / 2), rotate,
                                     1)
         img = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
-        return img
+        return img, angle
 
 
 def fliph_image(img, angle):
@@ -73,7 +75,7 @@ def fliph_image(img, angle):
         return cv2.flip(img, 1), angle
 
 
-def do_rotate(image, min=5, max=15, orientation='rand'):
+def do_rotate(image, angle, min=5, max=15, orientation='rand'):
         rows, cols, ch = image.shape
 
         # Randomly select a rotation angle from the range passed.
@@ -93,6 +95,17 @@ def do_rotate(image, min=5, max=15, orientation='rand'):
                                     rotation_angle, 1)
         return cv2.warpAffine(image, M,
                               (image.shape[1], image.shape[0])), -rotation_angle
+
+
+def enc_image_aug_maybe(image, angle, augmentation=None):
+        aug_image = image
+        aug_angle = angle
+        if callable(augmentation):
+                aug_image, aug_angle = augmentation(image, angle)
+        enc_aug_image = cv2.imencode('.jpg', aug_image)[1]
+
+        return augmentation.__name__ if augmentation else '', \
+               pickle.dumps({'image': enc_aug_image, 'angle': aug_angle})
 
 
 def main():
@@ -116,24 +129,37 @@ def main():
 
         csv_file_name = os.path.join(args.indir, 'training.data.csv')
         lmdb_name = args.outdir
-        map_size = int(8e9) * 2
+        augmentations = [None,  # Identity
+                         fliph_image,
+                         augment_brightness,
+                         image_blur,
+                         add_random_shadow,
+                         image_rotate,
+                         ]
 
-        with open(csv_file_name, newline='') as csvFile,\
-                    lmdb.open(lmdb_name, map_size=map_size).begin(write=True) as txn:
+        map_size = int(6e9) + int(3e9) * len(augmentations)
+
+        with open(csv_file_name, newline='') as csvFile, \
+                    lmdb.open(lmdb_name, map_size=map_size).begin(write=True) as txn, \
+                    Pool(max(len(augmentations), cpu_count() - 1)) as p:
                 lines_count = sum(1 for line in open(csv_file_name))
-                for row in tqdm(csv.DictReader(csvFile), total=(lines_count - 1)):
-                        filename = row['filename']
-                        img = cv2.imread(os.path.join(args.indir, filename))
-                        img_jpg = cv2.imencode('.jpg', img)[1]
-                        angle = float(row['angle'])
-                        txn.put(filename.encode(),
-                                pickle.dumps({'image': img_jpg, 'angle': angle}))
+                try:
+                        for row in tqdm(csv.DictReader(csvFile), total=(lines_count - 1)):
+                                filename = row['filename']
+                                angle = float(row['angle'])
+                                image = cv2.imread(os.path.join(args.indir, filename))
 
-                        filename += '+flipped'
-                        img_flipped, angle_flipped = fliph_image(img, angle)
-                        img_jpg = cv2.imencode('.jpg', img_flipped)[1]
-                        txn.put(filename.encode(),
-                                pickle.dumps({'image': img_jpg, 'angle': angle_flipped}))
+                                aug_data = p.map(partial(enc_image_aug_maybe,
+                                                         image,
+                                                         angle),
+                                                 augmentations)
+
+                                for aug, aug_dump in aug_data:
+                                        txn.put(str.encode(filename + '+' + aug),
+                                                aug_dump)
+                finally:
+                        p.close()
+                        p.join()
 
 
 if __name__ == '__main__':
